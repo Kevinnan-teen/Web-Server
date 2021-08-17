92475a81d95153fad2d3938b825c8259cf8cb12e
Kevinnan-teen <Kevinnan813@gmail.com>
2021-08-17 09:43:38 +0800

    add epoll http thread socket

---
+++ include/noncopyable.h
@@ -0,0 +1,21 @@
+//
+// Created by nachr on 2021/8/6
+//
+
+#pragma once
+
+//#ifndef WEBSERVER_NONCOPYABLE_H
+//#define WEBSERVER_NONCOPYABLE_H
+
+class noncopyable
+{
+public:
+    noncopyable(const noncopyable&) = delete;
+    noncopyable& operator=(const noncopyable&) = delete;
+
+protected:
+    noncopyable() = default;
+    ~noncopyable() = default;
+
+};
+//#endif //WEBSERVER_NONCOPYABLE_H
